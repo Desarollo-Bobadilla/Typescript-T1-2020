24c1a909853f17984652775472395d740935077c
Santiago Bobadilla <s.bobadilla@uniandes.edu.co>
2020-10-19 12:08:19 -0500

    Ya-Ha

---
+++ ts/dataCourses.ts
@@ -0,0 +1,12 @@
+import { Course } from './course.js';
+
+export const dataCourses = [
+  new Course("Ingeniería de Sw", "Jose Bocanegra", 3),
+  new Course("Modelos Probabilisticos", "Santiago Perez", 3),
+  new Course("Probabilidad y Estaditica II", "Carlos Caro", 3),
+  new Course("Control de Producción", "Camil Martinez", 3),
+  new Course("Infratec", "Jesse Padilla", 3),
+  new Course("TI", "Julian Moreno", 3),
+  new Course("Dinamica de sistemas", "María A. Victorino", 3),
+  new Course("Monitoria", "Carlos Lozano", 3)
+]
